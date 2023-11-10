3e7c4b9c251768413088eece63201541830cf6d7
hirushiRathnayake <hirushir99@gmail.com>
2023-11-10 21:39:33 +0530

    create home page strucure

---
+++ front-end/school/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import scl1 from '../Assets'
+
+function Home() {
+   const img1 = scl1;
+  return (
+    <div>
+    <img src='img1'/>
+    
+      
+    </div>
+  )
+}
+
+export default Home
