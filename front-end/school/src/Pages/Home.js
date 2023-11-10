c013245dfd332b040750196ed428ac92bef49829
hirushiRathnayake <hirushir99@gmail.com>
2023-11-10 21:54:03 +0530

    Add home descriptions

--- front-end/school/src/Pages/Home.js
+++ front-end/school/src/Pages/Home.js
@@ -5,8 +5,11 @@ function Home() {
    const img1 = scl1;
   return (
     <div>
-    <img src={img1}/>
+            <img src={img1}/>
+              <h1>Welcome To Thalatuoya Junior School </h1>
+               <span>This institute is located at THALTHUOYA . This institute's Education level is Prov.. It is in the Kandy district under Central provinence. Its education zone is Kandy under Pathahewaheta Educational division. It offers classes from G1-5. The total number of students in this institution is 912. There are 471 male and 441 female students in this institute</span>
     </div>
+
   )
 }
 
