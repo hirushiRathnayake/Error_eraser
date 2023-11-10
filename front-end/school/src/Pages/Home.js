cd8eeda99b8978968980656160d4b1520f1b8c88
hirushiRathnayake <hirushir99@gmail.com>
2023-11-10 21:50:48 +0530

    add image to home page

--- front-end/school/src/Pages/Home.js
+++ front-end/school/src/Pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import scl1 from '../Assets'
+import scl1 from '../Assets/scl1.png'
 
 function Home() {
    const img1 = scl1;
   return (
     <div>
-    <img src='img1'/>
-    
-      
+    <img src={img1}/>
     </div>
   )
 }
