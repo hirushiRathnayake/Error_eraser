7d71890cce36e50916e1c580ddb3739d8ba9554d
hirushiRathnayake <hirushir99@gmail.com>
2023-11-12 14:24:08 +0530

    create cards for seperate logins

--- front-end/school/src/App.js
+++ front-end/school/src/App.js
@@ -7,8 +7,11 @@ function App() {
   return (
     <div className="App">
    
-<BasicExample/>
+<BasicExample ctitle="Admin"  cContent="Are you an admin? If you are a admin, please log using below button"  cButton= "Admin Login"/>
 
+<BasicExample ctitle="Teacher"  cContent="Are you a Teacher? If you are a Teacher, please log using below button"  cButton= "Teacher Login"/>
+    
+<BasicExample ctitle="Student"  cContent="Are you a  student? If you are a student, please log using this button"  cButton= "student Login"/>   
     </div>
   );
 }
--- front-end/school/src/Components/BlockButton.js
+++ front-end/school/src/Components/BlockButton.js
@@ -1,17 +1,16 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function BasicExample() {
+function BasicExample(props) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.ctitle}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.cContent}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">{props.cButton}</Button>
       </Card.Body>
     </Card>
   );
